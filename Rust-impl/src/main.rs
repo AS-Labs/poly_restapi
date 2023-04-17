00407e3cb1efa38af5fe1f371749435a7ee05619
a.alshaiji <a.alshaiji@protonmail.com>
2023-04-17 07:17:01 +0300

    feat(main.rs): add Microservice struct and implement Service trait for it

--- Rust-impl/src/main.rs
+++ Rust-impl/src/main.rs
@@ -5,4 +5,21 @@ extern crate futures;
 extern crate log;
 extern crate env_logger;
 
+use hyper::server::{Request, Response, Service};
+
+use futures::future::Future;
+
+struct Microservice;
+
+impl Service for Microservice {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Futre = Box<Future<Item = Self::Response, Error = Self::Error>>;
+
+    fn call(&self, request: Request) -> Self::Future {
+        info!("Microservice recieved a request: {:?}", request);
+        Box::new(futures::future::ok(Response::new()))
+    }
+}
 
